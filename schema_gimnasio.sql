802937aa55a4165f7895bcfa0f056351d310e3ec
Carlos051403 <reyescarlos691@gmail.com>
2026-07-07 13:32:12 +0200

    refactorización: Optimización de memoria, aplicación de NOT NULL Y AUTO_INCREMENT

--- schema_gimnasio.sql
+++ schema_gimnasio.sql
@@ -1,32 +1,34 @@
+DROP DATABASE Gimnasio;
 CREATE DATABASE Gimnasio;
 USE Gimnasio;
 CREATE TABLE Socio (
-num_socio int,
-nombre varchar(255),
-apellido varchar(255),
-telefono varchar(255),
+num_socio int NOT NULL auto_increment,
+nombre varchar(50) NOT NULL,
+apellido varchar(50) NOT NULL,
+telefono varchar(20) NOT NULL,
 primary key (num_socio));
 
 CREATE TABLE Instructor (
-dni varchar(15),
-nombre varchar(255),
-especialidad varchar(255),
-correo varchar(255),
+dni varchar(15) NOT NULL,
+nombre varchar(50) NOT NULL,
+especialidad varchar(50) NOT NULL,
+correo varchar(100) NOT NULL,
 primary key (dni));
 
 CREATE TABLE Clase (
-id_clase varchar(255),
-nombre_actividad varchar(255),
-capacidad_max int,
-dni_instructor varchar(15),
+id_clase int NOT NULL auto_increment,
+codigo_publico varchar(50) UNIQUE,
+nombre_actividad varchar(50) NOT NULL,
+capacidad_max int NOT NULL,
+dni_instructor varchar(15) NOT NULL,
 primary key (id_clase),
 foreign key (dni_instructor) references Instructor (dni));
 
 CREATE TABLE Asiste (
-id_asistencia int,
+id_asistencia int NOT NULL auto_increment,
 primary key (id_asistencia),
-dni_id_socio int,
-clase_id varchar(255),
+dni_id_socio int NOT NULL,
+clase_id int NOT NULL,
 foreign key (dni_id_socio) references Socio (num_socio),
 foreign key (clase_id) references Clase (id_clase));
 
