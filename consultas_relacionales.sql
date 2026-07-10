0345de4f1c00f39fc0311038cc3d196580e37f87
Carlos051403 <reyescarlos691@gmail.com>
2026-07-10 14:57:19 +0200

    Actualización del script CRUD con nuevos datos y creación de consultas relacionales JOIN

--- consultas_relacionales.sql
+++ consultas_relacionales.sql
@@ -8,3 +8,17 @@ INNER JOIN clase
 ON id_clase = clase_id;
 
 DESCRIBE asiste;
+
+SELECT nombre, nombre_actividad
+FROM socio
+LEFT JOIN asiste
+ON num_socio = dni_id_socio
+LEFT JOIN clase
+ON id_clase = clase_id;
+
+SELECT nombre, nombre_actividad
+FROM socio
+RIGHT JOIN asiste
+ON num_socio = dni_id_socio
+RIGHT JOIN clase
+ON id_clase = clase_id;
--- crud_gimnasio.sql
+++ crud_gimnasio.sql
@@ -33,3 +33,37 @@ DELETE FROM socio
 WHERE num_socio = 5;
 
 SELECT * FROM socio;
+
+INSERT INTO asiste (dni_id_socio, clase_id)
+VALUES
+(1, 2),
+(2, 2);
+
+INSERT INTO instructor (dni, nombre, especialidad, correo)
+VALUES 
+('88888888D', 'Pep', 'Estratega', 'pep.mister@gmail.com'),
+('99999999A', 'Ancelotti', 'Estratega', 'Carletto@gmail.com');
+
+SELECT * FROM socio;
+
+INSERT INTO socio (nombre, apellido, telefono)
+VALUES 
+('Andres', 'Iniesta', '11122233'),
+('Kylian', 'Mbappe', '99988877');
+
+INSERT INTO clase (codigo_publico, nombre_actividad, capacidad_max, dni_instructor)
+VALUES
+('C6', 'Táctica', 20, '88888888D'),
+('C7', 'Visión de Juego', 15, '88888888D'),
+('C8', 'Arrastre de marcas', 9, '99999999A');
+
+INSERT INTO asiste (dni_id_socio, clase_id)
+VALUES
+(3, 3),
+(3, 4),
+(3, 6),
+(4, 1),
+(4, 7),
+(6, 7);
+
+SELECT * FROM asiste;
